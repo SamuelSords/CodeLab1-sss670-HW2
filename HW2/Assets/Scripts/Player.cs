e1444b700bdd47cd1f2743e525fc1010a226c255
SamuelSords <54908881+SamuelSords@users.noreply.github.com>
2020-03-08 20:21:20 -0400

    Added Characters and animation

--- HW2/Assets/Scripts/Player.cs
+++ HW2/Assets/Scripts/Player.cs
@@ -13,6 +13,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // move character
         transform.Translate(Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f, 0f);
+
+        // flip character
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -10;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 10;
+        }
+        transform.localScale = characterScale;
     }
 }
